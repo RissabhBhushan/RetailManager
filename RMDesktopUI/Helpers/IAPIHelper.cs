48c00b2a5299ff5f21618df685d4f2b303355680
RissabhBhushan <32068552+RissabhBhushan@users.noreply.github.com>
2019-05-12 08:00:54 +0530

    WireUp Login Form to API
    Connects the login from button to the authenticate API endpint (/token). Get back the bearer token or an exception if failed.

---
+++ RMDesktopUI/Helpers/IAPIHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using RMDesktopUI.Models;
+
+namespace RMDesktopUI.Helpers
+{
+    public interface IAPIHelper
+    {
+        Task<AuthenticatedUser> Authenticate(string username, string password);
+    }
+}
